8e7c2918feee1eee55538ff7049bc6326f69b896
alex <o.lototskyi@gmail.com>
2023-08-26 22:34:08 +0300

    connect google maps

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,12 +1,21 @@
 import { Modal } from "./UI/Modal";
+import { Map } from "./UI/Map";
 
 class PlaceFinder {
     constructor(props) {
         const addressForm = document.querySelector("form");
         const locateUserBtn = document.getElementById("locate-btn");
 
-        locateUserBtn.addEventListener("click", this.locateUserHandler);
-        addressForm.addEventListener("submit", this.findAddressHandler);
+        locateUserBtn.addEventListener("click", this.locateUserHandler.bind(this));
+        addressForm.addEventListener("submit", this.findAddressHandler.bind(this));
+    }
+
+    selectPlace(coordinates) {
+        if (this.map) {
+            this.map.render(coordinates);
+        } else {
+            this.map = new Map(coordinates);
+        }
     }
 
     locateUserHandler() {
@@ -17,7 +26,10 @@ class PlaceFinder {
             return;
         }
 
-        const modal = new Modal('loading-modal-content', 'Loading location - please wait!');
+        const modal = new Modal(
+            "loading-modal-content",
+            "Loading location - please wait!"
+        );
         modal.show();
 
         navigator.geolocation.getCurrentPosition(
@@ -27,7 +39,7 @@ class PlaceFinder {
                     lat: successResult.coords.latitude,
                     lng: successResult.coords.longitude,
                 };
-                console.log(coordinates);
+                this.selectPlace(coordinates);
             },
             (error) => {
                 modal.hide();
